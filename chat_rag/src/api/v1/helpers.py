f07aaef147b403e7212c29d5a331d1e0cb5892b9
Filipe Theodoro <ffatheodoro@gmail.com>
2024-06-11 20:32:37 -0300

    refact: split call rag functions

--- chat_rag/src/api/v1/helpers.py
+++ chat_rag/src/api/v1/helpers.py
@@ -1,3 +1,4 @@
+from typing import Union, AsyncGenerator
 from src.services.llms import ollama 
 from src.services.retriever import retriever
 from src.api.v1.models import ChatRequest
@@ -8,7 +9,7 @@ async def call_rag(request: ChatRequest):
     documents = await retriever.asearch(
         query=request.text,
         collection_name=request.collection_name,
-        limit=5
+        limit=2
     )
     messages = [
         {
@@ -20,7 +21,25 @@ async def call_rag(request: ChatRequest):
             "content": PROMPT.format(DOCUMENTS=documents, QUESTION=request.text)
         }
     ]
-    return ollama.call_llm(
+    return await ollama.call_llm(
         messages=messages,
-        stream=request.stream
     )
+
+async def call_rag_stream(request: ChatRequest):
+    documents = await retriever.asearch(
+        query=request.text,
+        collection_name=request.collection_name,
+        limit=2
+    )
+    messages = [
+        {
+            "role": "system",
+            "content": "You are a RAG chatbot, only answer the QUESTION with the provided DOCUMENTS, else say you cant help in a polite away. You must never talk about religion, politics, or provide any harm. Answer in the same language as QUESTION."
+        },
+        {
+            "role": "user",
+            "content": PROMPT.format(DOCUMENTS=documents, QUESTION=request.text)
+        }
+    ]
+    async for token in ollama.call_llm_stream(messages=messages):
+        yield token
