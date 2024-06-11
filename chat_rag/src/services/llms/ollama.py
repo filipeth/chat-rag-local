4b97082c0d3acbc255fd19ccc348f91383f46729
Filipe Theodoro <ffatheodoro@gmail.com>
2024-06-11 20:29:27 -0300

    refact: split call llm functions

--- chat_rag/src/services/llms/ollama.py
+++ chat_rag/src/services/llms/ollama.py
@@ -8,7 +8,7 @@ LLM_API_URL = "http://localhost:11434/api/chat"
 LLM_API_URL = "http://ollama:11434/api/chat"
 
 
-async def call_llm(messages: list[dict], model: str = "phi3:mini", stream: bool=True):
+async def call_llm_stream(messages: list[dict], model: str = "phi3:mini"):
     headers = {
         "Content-Type": "application/json",
         "Authorization": f"Bearer {os.getenv('LLM_API_KEY', 'teste')}"
@@ -16,28 +16,36 @@ async def call_llm(messages: list[dict], model: str = "phi3:mini", stream: bool=
     data = {
         "model": model,
         "messages": messages,
-        "stream": stream
+        "stream": True,
+        "options": {
+            "temperature": 0
+        }
     }
     async with aiohttp.ClientSession() as session:
         async with session.post(LLM_API_URL, headers=headers, json=data) as response:
-            if stream:
-                async for line in response.content:
-                    token = re.search(r'(?<=content\":\").*?(?=\"},)', line.decode('utf-8'))
-                    if token:
-                        print(token.group(), end="")
-                    else:
-                        print(line.decode('utf-8'))
-            else:
-                return await response.json()
-            
-if __name__ == "__main__":
-    resp = asyncio.run(call_llm([
-            {
-                "role": "system",
-                "content": "You are a helpful assistant."
-            },
-            {
-                "role": "user",
-                "content": "Hello!"
-            }
-        ]))
+            async for line in response.content:
+                token = re.search(r'(?<=content\":\").*?(?=\"},)', line.decode('utf-8'))
+                if token:
+                    yield token
+                else:
+                    print(line.decode('utf-8'))
+    yield "\n"
+
+async def call_llm(messages: list[dict], model: str = "phi3:mini"):
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {os.getenv('LLM_API_KEY', 'teste')}"
+    }
+    data = {
+        "model": model,
+        "messages": messages,
+        "stream": False,
+        "options": {
+            "temperature": 0
+        }
+    }
+    async with aiohttp.ClientSession() as session:
+        async with session.post(LLM_API_URL, headers=headers, json=data) as response:
+            resp = await response.json()
+            print(resp)
+            return resp
