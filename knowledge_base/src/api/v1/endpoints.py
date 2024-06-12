f7c16e921c071f951d6e7b79cd8b4a46d8fc8896
Filipe Theodoro <ffatheodoro@gmail.com>
2024-06-12 17:11:00 -0300

    refact: async vectorize endpoint

--- knowledge_base/src/api/v1/endpoints.py
+++ knowledge_base/src/api/v1/endpoints.py
@@ -1,33 +1,17 @@
-from fastapi import APIRouter, File, UploadFile
-from pydantic import BaseModel
-
-from src.vector_db import vector_db
-from src.utils.utils import load_html
+from fastapi import APIRouter, UploadFile
+from src.api.v1.helpers import call_vectorize, call_search
 
 router = APIRouter()
 
 @router.post("/vectorize")
-def vectorize(collection_name: str, file: UploadFile = None):
-
-    if file:
-        try:
-            docs = load_html(file=file.file)
-        except:
-            return "error"
-    else:
-        return "error"
-        docs = load_html(data=request.text)
-
-    vector_db.add_documents(
-        collection_name=collection_name,
-        documents=docs
-    )
-
+async def vectorize(collection_name: str, file: UploadFile = None):
+    await call_vectorize(collection_name, file)
+    
     return {"message": "Document vectorized and added to database"}
 
 @router.get("/search")
 def search(query: str, collection_name: str, limit: int = 10):
-    return vector_db.search(
+    return call_search(
         text=query,
         collection_name=collection_name,
         limit=limit
